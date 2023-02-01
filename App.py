d3bd61a094503193ad3c0cfd6009c419638b5f4c
JohnySmid <56623847+JohnySmid@users.noreply.github.com>
2023-02-01 19:55:50 +0100

    Update App.py
    komentáře

--- App.py
+++ App.py
@@ -18,45 +18,57 @@ class ScoreTracker:
     def __init__(self):
         self.score = 100
 
+    # odecita skore
     def deduct_score(self, update):
         self.score -= update
 
+    #prida skore
     def addup_score(self, update):
         self.score += update
 
+    #vrati skore
     def get_score(self):
         return self.score
 
+# Layout
 class MyLayout(Widget):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.playstart()
+        # inicializace ScoreTrackeru()
         self.skore = ScoreTracker()
+        # nastavi text labelu na nase urcene skore
         self.ids.label_score.text = str(self.skore.get_score())
 
     # pro debug ucely, jsem si udelal fci na "start", zbytecna ale pouzivam ji
     def playstart(self):
         self.start()
 
+    # funkce na hru
     def start(self):
         # init decku, s kazdym kolem se udela novy deck
         self.deck = Deck.Deck()
+        # zamicha deck
         self.deck.shuffle()
+
+        # promene slouzici ke hre
         self.hit = False
         self.played = False
         self.bet = False
         self.castkabet = 0
 
-        # kivy
+        # kivy widgety
         self.ids.img1karty.source = ''
         self.ids.img2karty.source = ''
         self.ids.img3karty.source = ''
         self.ids.label_bet.text = ''
 
 
-        # karty
+        # inicializace ruky hrace a dealera
         self.player_hand = Hand.Hand()
         self.dealer_hand = Hand.Hand()
+
+        # prida karty do ruky hrace a dealera
         self.player_hand.add_card(self.deck.deal())
         self.player_hand.add_card(self.deck.deal())
         self.dealer_hand.add_card(self.deck.deal())
@@ -67,15 +79,19 @@ class MyLayout(Widget):
         # txt karet
         self.ids.img1karty.source = f'txtkarty/{str(self.player_hand.cards[0])}.png'
         self.ids.img2karty.source = f'txtkarty/{str(self.player_hand.cards[1])}.png'
+
+        # label pod texturami karet
         self.ids.label1.text = str(
             f"Your hand: {self.player_hand.cards[0]} {self.player_hand.cards[1]}  |  val: {self.player_hand.get_value()}")
 
 
 
-    # fce pro pridavani sazek
+    # f-ce pro pridavani sazek
     def Addup(self):
         # uz jednou vsadil
         if self.bet == True:
+            # PopUp - definovan na zacatku .kv souboru
+            # widgety to jiz obsahuje, menime pouze jejich hodnoty
             PUzVsazeno = Factory.MyPopup()
             PUzVsazeno.ids.Pmessage.text = 'Uz jste si jednou vsadil, dejte hit nebo stand'
             PUzVsazeno.ids.Pbutton.text = 'zavrit'
@@ -100,6 +116,7 @@ class MyLayout(Widget):
         # sazka
         elif self.bet == False and self.ids.text_input.text != '':
             self.bet = True
+            # pokud zadame vice nez mame zetonu
             if self.skore.get_score() < int(self.ids.text_input.text):
                 self.bet = False
                 PMocZetonu = Factory.MyPopup()
@@ -111,10 +128,12 @@ class MyLayout(Widget):
                 self.skore.deduct_score(int(self.ids.text_input.text))
                 self.castkabet = int(self.ids.text_input.text)
                 self.ids.label_bet.text = str(self.castkabet)
+
+                # pokud mame 0 a min zetonu
                 if self.skore.get_score() <= 0:
                     self.bet = False
+                    # musime pridat skore, jelikoz uz jsme odecetli
                     self.skore.addup_score(int(self.ids.text_input.text))
-                    #self.ids.label_bet.text = ''
                     PZeroZetonu = Factory.MyPopup()
                     PZeroZetonu.ids.Pmessage.text = 'Nemuzete mit 0 zetonu, zadejte mensi castku'
                     PZeroZetonu.ids.Pbutton.text = 'zavrit'
@@ -124,9 +143,11 @@ class MyLayout(Widget):
 
         self.ids.text_input.text = ''
 
-    # fce pro dalsi kartu
+    # f-ce pro dalsi kartu
     def Hit(self):
+        # pokud vsazeno
         if self.bet == True:
+            # pokud chceme dalsi kartu s BlackJackem v ruce
             if self.player_hand.get_value() == 21:
                 PBJ = Factory.MyPopup()
                 PBJ.ids.Pmessage.text = 'Mate BlackJack! Nechcete dalsi kartu'
@@ -135,12 +156,16 @@ class MyLayout(Widget):
 
             elif self.hit == False:
                 self.hit = True
+                # prida kartu
                 self.player_hand.add_card(self.deck.deal())
-                #self.player_hand.DEBUG_LISTCARDS()
+                # self.player_hand.DEBUG_LISTCARDS()
+                # vypis listu s mezerou
                 self.ids.label1.text = str(
                     f"Your hand: {' '.join(str(card) for card in self.player_hand.cards)}  |  val: {self.player_hand.get_value()}")
-                # print(str(self.player_hand.cards[2]))
+
                 self.ids.img3karty.source = f'txtkarty/{str(self.player_hand.cards[2])}.png'
+
+                # Bust
                 if self.player_hand.get_value() > 21:
                     PBust = Factory.MyPopup()
                     PBust.ids.Pmessage.text = 'Bust!'
@@ -162,16 +187,16 @@ class MyLayout(Widget):
             PVsad.open()
 
 
-    # fce pro ukazani vysledku
+    # f-ce pro ukazani vysledku
     def Stand(self):
         if self.bet == True:
+            # pop up
             PStand = Factory.MyPopup()
             PStand.ids.Pbutton.text = 'zavrit'
 
             # check v pripade vyhry
             if self.player_hand.get_value() > self.dealer_hand.get_value() and self.player_hand.get_value() < 21:
                 self.skore.addup_score(self.castkabet*2)
-                #print(f'U win with {self.player_hand.get_value()} points, delears hand was {self.dealer_hand.get_value()}')
                 PStand.ids.Pmessage.text = f'Vyhravate! S hodnotou {self.player_hand.get_value()}'
 
             # mozny blackjack
@@ -216,6 +241,8 @@ class MyLayout(Widget):
 
             PStand.open()
             self.ids.label_score.text = str(self.skore.get_score())
+
+            # spusti se nova hra
             self.playstart()
 
 
@@ -224,5 +251,3 @@ class MyLayout(Widget):
             PVsad.ids.Pmessage.text = 'Nejdrive vsadte castku'
             PVsad.ids.Pbutton.text = 'zavrit'
             PVsad.open()
-
-
