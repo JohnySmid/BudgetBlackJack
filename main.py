f79175dcca54c1dbe876707d48265a6a497eb236
JohnySmid <56623847+JohnySmid@users.noreply.github.com>
2023-02-01 19:50:43 +0100

    Update main.py
    komentáře

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from kivy.app import App
 from App import *
 
-
+# Class pro spusteni GUI
 class BudgetBlackJackApp(App):
     def build(self):
         return MyLayout()
 
 if __name__ == '__main__':
     BudgetBlackJackApp().run()
-
-
