7eb5eec8c26db6dfff76b1598e416143b4858277
JohnySmid <56623847+JohnySmid@users.noreply.github.com>
2023-02-01 19:54:30 +0100

    Update Hand.py
    komentáře

--- Hand.py
+++ Hand.py
@@ -6,19 +6,25 @@ class Hand:
         #print(f'karta: {card}')
         self.cards.append(card)
 
+    # vraci hodnotu
     def get_value(self):
         value = 0
         num_aces = 0
+        # projde to karty v ruce
         for card in self.cards:
+            # jestli je to J Q K, tak +10
             if card.rank in ['J', 'Q', 'K']:
                 value += 10
+            # pokud eso, tak zatim +1
             elif card.rank == 'A':
                 value += 1
                 num_aces += 1
+            # jinak +rank karty
             else:
                 value += int(card.rank)
 
         # vice es v ruce
+        # tj. jestli v ruka je min jak 21, pak hodnota esa je +10, jinak +1
         while num_aces > 0 and value + 10 <= 21:
             value += 10
             num_aces -= 1
