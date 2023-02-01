ac183437f5d904f68e898e7f95ebdfe2de8e09be
JohnySmid <56623847+JohnySmid@users.noreply.github.com>
2023-02-01 19:54:59 +0100

    Update Deck.py
    komentáře

--- Deck.py
+++ Deck.py
@@ -3,6 +3,7 @@ import random
 
 class Deck:
     def __init__(self):
+        # vytvori to balik se vsemi kartami z definovanych listu v Cards.py
         self.cards = [Cards.Card(rank, suit) for rank in Cards.ranks for suit in Cards.suits]
 
     def shuffle(self):
