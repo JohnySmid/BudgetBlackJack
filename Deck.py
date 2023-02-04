6d7c6e0b912d42b470b80b8319b32db83f5b4f56
JohnySmid <56623847+JohnySmid@users.noreply.github.com>
2023-02-04 09:46:47 +0100

    Update Deck.py
    docstrings

--- Deck.py
+++ Deck.py
@@ -3,15 +3,20 @@ import random
 
 class Deck:
     def __init__(self):
-        # vytvori to balik se vsemi kartami z definovanych listu v Cards.py
+        """Inicializace balíčku s všemi kartami z definovaných seznamů v Cards.py."""
         self.cards = [Cards.Card(rank, suit) for rank in Cards.ranks for suit in Cards.suits]
 
     def shuffle(self):
+        """Zamíchání karet v balíčku."""
         random.shuffle(self.cards)
 
     def deal(self):
-        #z konce listu popuje itemy
+        """Rozdání karty z balíčku.
+
+            Vrátí poslední kartu z balíčku a odebere ji z něj.
+        """
         return self.cards.pop()
 
     def DEBUG_PRINT_DECK(self):
+        """Debugovací metoda výpisu karet v balíčku."""
         print(*self.cards, sep=" ")
